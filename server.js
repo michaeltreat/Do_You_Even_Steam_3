67475fdc85bdcd2065f57a8cddb7e9199b2d0fc4
MichaelTreat <michael.leon.treat@gmail.com>
2018-01-24 18:07:29 -0800

    Updated and dried up the current server code. No new EPs added.

--- server.js
+++ server.js
@@ -9,70 +9,58 @@ const bodyParser = require('body-parser').urlencoded({extended: true}) // To par
 const PORT = process.env.PORT || 3000
 const _API_ = 'https://api.steampowered.com' 
 
+
+// Connect to Database
 const client = new pg.Client('postgres://postgres:12341234@localhost:5432/dyes')
 client.connect()
 client.on('error', error => console.log('Error connecting to DB:', error))
 
+// Initialize app.
 const app = express()
 app.use(cors()) // Open to all requests.
 
 // ----------- SteamAPI Endpoints ---------------
 
 app.get('/api/v1/steamers/vanityurl/:name', (req, res) => {
-  console.log(`Hit get /steamers/vanityurl/${req.params.name}`)
+  console.log(`Hit GET /steamers/vanityurl/${req.params.name}`)
   superAgent.get(`${_API_}/ISteamUser/ResolveVanityURL/v0001/?key=${process.env.KEY}&vanityurl=${req.params.name}`)
-    .then( result => {
-      console.log('Sending results for /steamers/vanityurl/:name')
-      res.send(JSON.parse(result.text).response.steamid)
-    })
+    .then( result => res.send(result.text.response.steamid))
+    .catch( err => res.send('there was an error in the GET /vanityurl endpoint', err))
 })
 
 app.get('/api/v1/steamers/:id', (req, res) =>{
-  console.log(`Hit get /steamers/${req.params.id}`)
-
+  console.log(`Hit GET /steamers/${req.params.id}`)
   superAgent.get(`${_API_}/IPlayerService/GetOwnedGames/v0001/?key=${process.env.KEY}&steamid=${req.params.id}&format=json&include_appinfo=1`)
-    .then( result => {
-      console.log('Sending results for /steamers/:id')
-      res.send(result)
-    }).catch(err => console.log('Err in /steamers/:id :', err))
+    .then( result => res.send(result))
+    .catch(err => console.log( err))
 })
 
 // --------------- Leaderboard Endpoints ----------------
 
 app.get('/api/v1/leaderboard', (req, res) =>{
-  console.log('Hit get /leaderboard.')
+  console.log('Hit GET /leaderboard.')
   client.query('SELECT * FROM leaderboard')
-    .then( data => {
-      console.log('Sending results for get /leaderboard.')
-      res.send(data.rows)
-    })
+    .then( data => res.send(data.rows))
+    .catch( err => console.log(err))
 })
 
 app.post('/api/v1/leaderboard', bodyParser, (req, res) => {
-  console.log('Hit Post /leaderboard')
+  console.log('Hit POST /leaderboard')
   client.query(`INSERT INTO leaderboard(name, hours) VALUES('${req.body.name}', ${req.body.hours})`)
-  res.send('Insert Successful.')
+    .then(() => res.send('Insert Successful.'))
+    .catch( err => console.log(err))
 })
-
-asdfasdf2asdfa
-
-
 
 
 // --------------- Other Endpoints -----------------------
 
-app.get('/test', (req, res) => {
-  console.log('Test route hit.')
-  res.send( 'Successful hit on test route.')
-})
-
 app.get('/api/v1/accounts', (req, res) => {
-  console.log('Hit /accounts.')
+  console.log('Hit GET /accounts.')
   client.query('SELECT COUNT(*) FROM accounts').then(results => res.send(results.rows))
+    .then(results => res.send(results))
+    .catch( err => console.log(err))
 })
 
 
-
-
-
+app.get('/ping', (req, res) => res.send( 'pong'))
 app.listen(PORT, () => console.log('Connected on port:', PORT))
